447d1726b6face7bc50877e4af4ef4ca2c875a9c
unknown <jikjik993@gmail.com>
2016-04-30 22:32:56 +0800

    Q2.py

---
+++ Q2.py
@@ -0,0 +1,10 @@
+Queue = [] #using list as a Queue
+
+Queue.insert(0,'melon') #enqueue of Queue
+print(Queue)
+Queue.insert(0,333) #enqueue of Queue
+print(Queue)
+size = len(Queue) #size of Queue
+print(size)
+Queue.pop() #dequeue of Queue
+print(Queue)
